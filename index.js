924d2c9dd84d6c3e41d2b34672ae4928ac3565b9
Md. Nasim Reza <61214781+nasimit2020@users.noreply.github.com>
2024-01-11 10:38:29 +0600

    Redux action create for dispatch

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+
 // state
 const initialCounterState = {
   count: 0,
@@ -7,4 +12,27 @@ const initialUsersState = {
   users: [{ name: "nasim reza" }],
 };
 
-// action
+// action - Object - 2 importance - type, payload
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload: { name: "Parvez Hossen" },
+  };
+};
+
+// 1. state
+// 2. dispatch action
+// 3. reducer
+// 4. store
